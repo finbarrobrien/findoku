93b626a433a4707b3f811a3e28a19ebfe7ded63b
finbarrobrien <finbarr.obrien@gmail.com>
2017-09-10 14:13:04 +0100

    More styling

--- src/components/grid/Cell.js
+++ src/components/grid/Cell.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {css} from 'glamor';
 
-const s = {
-  cell: css({
+const Cell = ({ row, col, value, activeCell, onCellClick }) => {
+  const highlight = activeCell.row === row || activeCell.col === col;
+  const select = activeCell.row === row && activeCell.col === col;
+  const selectedValue = activeCell.value === value;
+
+  const cellStyle = {
     flex: '0 0 auto',
     width: '45px',
     height: '45px',
@@ -11,20 +14,15 @@ const s = {
     lineHeight: '45px',
     textAlign: 'center',
     fontFamily: 'Verdana, Geneva, sans-serif',
-  }),
-  select: 'border border-dark bg-primary',
-  selectedValue: 'border border-dark bg-info',
-  highlight: 'border border-dark bg-secondary',
-  normal: 'border border-dark bg-light',
-};
-
-const Cell = ({ row, col, value, activeCell, onCellClick }) => {
-  const highlight = activeCell.row === row || activeCell.col === col;
-  const select = activeCell.row === row && activeCell.col === col;
-  const selectedValue = activeCell.value === value;
+    borderTop: row % 3 === 0 ? '2px solid #0d0d0d' : '1px solid grey',
+    borderLeft: col % 3 === 0 ? '2px solid #0d0d0d' : '1px solid grey',
+    borderBottom: row % 3 === 2 ? '2px solid #0d0d0d' : '1px solid grey',
+    borderRight: col % 3 === 2 ? '2px solid #0d0d0d' : '1px solid grey',
+    backgroundColor: select ? '#6666ff' : (highlight ? '#b3b3ff' : ( selectedValue ? '#4dff4d' : '#ffffff'))
+  };
   return (
-    <div className={ select ? s.select : (highlight ? s.highlight : ( selectedValue ? s.selectedValue : s.normal)) }
-         { ...s.cell }
+    <div
+         style={ cellStyle }
        onClick={() => onCellClick({ row, col, value })}>
     { value }
   </div>
